921f0051b198632298edaadff8d4970b93230a42
Rifqi Al Fatih <rifqialfatih@gmail.com>
2020-09-02 15:35:04 +0200

    Parameterized run

--- rl_learner/Learner_v1.py
+++ rl_learner/Learner_v1.py
@@ -18,9 +18,16 @@ try:
     import selectors
 except ImportError:
     import selectors2 as selectors  # run  python -m pip install selectors2
-host = '172.19.0.1'
-port = 65432
+import argparse
+
+parser = argparse.ArgumentParser(description='Set learner host port.')
+parser.add_argument('--host', default='172.19.0.1', help='Host')
+parser.add_argument('--port', default='65432', help='Port')
+args = parser.parse_args()
+
 sel = selectors.DefaultSelector()
+host = args.host
+port = args.port
 
 
 device = torch.device('cuda')
