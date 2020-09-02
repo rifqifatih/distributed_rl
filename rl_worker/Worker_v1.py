c499c39c37d1705a3acc9d64dc128740adab45e3
Rifqi Al Fatih <rifqialfatih@gmail.com>
2020-09-02 15:59:35 +0200

    Reorder build, change port to int

--- rl_worker/Worker_v1.py
+++ rl_worker/Worker_v1.py
@@ -28,7 +28,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Set learner host port.')
 parser.add_argument('--host', default='172.19.0.1', help='Host')
-parser.add_argument('--port', default='65432', help='Port')
+parser.add_argument('--port', default=65432, help='Port')
 args = parser.parse_args()
 
 sel = selectors.DefaultSelector()
